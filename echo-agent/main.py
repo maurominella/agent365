5fd752a10f21147c59741b24a18cfe97f659ca9d
Mauro Minella <mauro.minella@microsoft.com>
2026-01-20 01:45:25 +0100

    agent365

--- echo-agent/main.py
+++ echo-agent/main.py
@@ -8,11 +8,19 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+# Pydantic models, e.g. data models for request and response bodies
 class EchoRequest(BaseModel):
     message: str
 
 class EchoResponse(BaseModel):
     echo: str
+
+# Example usages of Pydantic model instantiation
+my_dict = {"echo": "Dict1"}
+x = EchoResponse(echo="Test")
+y = EchoResponse(**my_dict)
+z = EchoResponse(**{"echo": "Dict2"})
+
 
 @app.post("/echo", response_model=EchoResponse)
 def echo(req: EchoRequest):
@@ -23,6 +31,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="127.0.0.1",
-        port=8000,
+        port=80,
         reload=True
     )
